6a8e1542252cf556e1f8a59f1986ad8d29272d08
bakunovgerman <german.bakunov@gmail.com>
2021-08-12 22:53:35 +0700

    add MovieDao.kt

---
+++ app/src/main/java/com/example/homework_2_mts/repository/database/dao/MovieDao.kt
@@ -0,0 +1,18 @@
+package com.example.homework_2_mts.repository.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.homework_2_mts.repository.database.Movie
+
+@Dao
+interface MovieDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addMovies(movies: List<Movie>)
+
+    @Query("SELECT * FROM movies ORDER BY id ASC")
+    fun getMovies(): LiveData<List<Movie>>
+}
