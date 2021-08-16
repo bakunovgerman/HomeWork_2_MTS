e53478ade91f47a520dc4bdd6e892d006c8cc41f
bakunovgerman <german.bakunov@gmail.com>
2021-08-16 22:40:46 +0700

    вся работа с view выполняется в OnCreateView() MainFragment.kt

--- app/src/main/java/com/example/homework_2_mts/fragments/MainFragment.kt
+++ app/src/main/java/com/example/homework_2_mts/fragments/MainFragment.kt
@@ -66,13 +66,6 @@ class MainFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
 
-        init(view)
-        mainFragmentViewModel.loadData()
-
-        return view
-    }
-
-    private fun init(view: View) {
         swipeRefresh = view.findViewById(R.id.swipe_refresh)
         rvMovies = view.findViewById(R.id.rvMovies)
         rvPopularNow = view.findViewById(R.id.rvPopularNow)
@@ -116,6 +109,9 @@ class MainFragment : Fragment() {
         )
         mainFragmentViewModel.viewState.observe(viewLifecycleOwner, Observer(::setViewState))
         mainFragmentViewModel.updateMoviesList.observe(viewLifecycleOwner, Observer(::updateData))
+        mainFragmentViewModel.loadData()
+
+        return view
     }
 
     private fun initPopularNowData(popularNowItems: List<PopularNowDto>?) {
