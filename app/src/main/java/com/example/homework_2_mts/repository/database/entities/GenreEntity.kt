b7a99e0957be21545acef08db9eb5e31f8360dd4
bakunovgerman <german.bakunov@gmail.com>
2021-08-16 23:22:19 +0700

    create GenreDao.kt && GenreEntity.kt

--- app/src/main/java/com/example/homework_2_mts/repository/database/entities/GenreEntity.kt
+++ app/src/main/java/com/example/homework_2_mts/repository/database/entities/GenreEntity.kt
@@ -1,3 +1,14 @@
 package com.example.homework_2_mts.repository.database.entities
 
-data class GenreEntity(val name: String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "genres")
+data class GenreEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long,
+    @ColumnInfo(name = "name")
+    val name: String
+)
